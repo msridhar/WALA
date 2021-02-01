d0291a77407796cd08a3d239e282d991aed23fbc
Manu Sridharan <msridhar@gmail.com>
2021-02-01 08:45:28 -0800

    add a comment

--- com.ibm.wala.cast/src/main/java/com/ibm/wala/cast/tree/CAstSourcePositionMap.java
+++ com.ibm.wala.cast/src/main/java/com/ibm/wala/cast/tree/CAstSourcePositionMap.java
@@ -43,6 +43,8 @@ public interface CAstSourcePositionMap {
      */
     default String prettyPrint() {
       String file = getURL().getFile();
+      // TEMPORARY HACK: don't truncate the file path, to ensure uniqueness
+      // we'd like a proper fix here that doesn't use absolute paths
       // file = file.substring(file.lastIndexOf('/') + 1);
 
       int line = getFirstLine(), start_offset = getFirstOffset(), end_offset = getLastOffset();
