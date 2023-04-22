4c2d286c134f4ad85360d0286360fcd48bc9ec5f
Manu Sridharan <msridhar@gmail.com>
2023-04-22 08:55:12 -0700

    broaden fix

--- cast/js/src/main/java/com/ibm/wala/cast/js/translator/JSAstTranslator.java
+++ cast/js/src/main/java/com/ibm/wala/cast/js/translator/JSAstTranslator.java
@@ -260,14 +260,11 @@ public class JSAstTranslator extends AstTranslator {
     int nm = context.currentScope().getConstantValue('L' + composeEntityName(context, fn));
     // "Function" is the name we use to model the constructor of function values
     int tmp = super.doGlobalRead(n, context, "Function", JavaScriptTypes.Function);
-    Position old = null;
-    if (n.getKind() == CAstNode.FUNCTION_STMT) {
-      // For function statements, set the current position (used for the constructor invocation
-      // instruction added below) to be the location of the statement itself.
-      old = getCurrentPosition();
-      CAstEntity entity = (CAstEntity) n.getChild(0).getValue();
-      currentPosition = entity.getPosition();
-    }
+    // Set the current position (used for the constructor invocation instruction added below) to be
+    // the location of the function statement / expression itself.
+    Position old = getCurrentPosition();
+    CAstEntity entity = (CAstEntity) n.getChild(0).getValue();
+    currentPosition = entity.getPosition();
     try {
       context
           .cfg()
@@ -282,7 +279,7 @@ public class JSAstTranslator extends AstTranslator {
                       new DynamicCallSiteReference(
                           JavaScriptMethods.ctorReference, context.cfg().getCurrentInstruction())));
     } finally {
-      // Reset the current position if it was temporarily updated for a function statement
+      // Reset the current position
       if (old != null) {
         currentPosition = old;
       }
